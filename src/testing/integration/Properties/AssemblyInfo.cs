99f3e8467a434cf50c12639bcdf297113a9c40d0
Alan Quillin <alan.quillin@rackspace.com>
2012-11-13 22:59:59 -0600

    Added functional tests for currently implemented Rackspace Compute services

--- src/testing/integration/Properties/AssemblyInfo.cs
+++ src/testing/integration/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ui")]
+[assembly: AssemblyTitle("integration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ui")]
+[assembly: AssemblyProduct("integration")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,25 +19,8 @@ using System.Windows;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-//In order to begin building localizable applications, set 
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page, 
-    // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page, 
-    // app, or any theme specific resource dictionaries)
-)]
-
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b334cd3e-4cdf-4927-a079-1ef7f7d21d99")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -50,6 +31,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/testing/integration/Providers/Rackspace/ComputeTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using net.openstack.Core.Domain;
+
+namespace Net.OpenStack.Testing.Integration.Providers.Rackspace
+{
+    /// <summary>
+    /// Summary description for ComputeTests
+    /// </summary>
+    [TestClass]
+    public class ComputeTests
+    {
+        public ComputeTests()
+        {
+            _testIdentity = new CloudIdentity()
+            {
+                APIKey = ConfigurationManager.AppSettings["TestIdentityAPIKey"],
+                Password = ConfigurationManager.AppSettings["TestIdentityPassword"],
+                Region = ConfigurationManager.AppSettings["TestIdentityRegion"],
+                Username = ConfigurationManager.AppSettings["TestIdentityUserName"],
+            };
+        }
+
+        private TestContext testContextInstance;
+        private static CloudIdentity _testIdentity;
+        private static NewServer _testServer;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Test1_Should_Create_A_New_Server_In_DFW()
+        {
+            var provider = new net.openstack.Providers.Rackspace.ComputeProvider();
+            _testServer = provider.CreateServer("test-server", "My Test Server", "d531a2dd-7ae9-4407-bb5a-e5ea03303d98", "2", _testIdentity);
+
+            Assert.IsNotNull(_testServer);
+            Assert.IsNotNull(_testServer.Id);
+        }
+
+        [TestMethod]
+        public void Test2_Should_Get_Details_For_Newly_Created_Server_In_DFW()
+        {
+            var provider = new net.openstack.Providers.Rackspace.ComputeProvider();
+            var serverDetails = provider.GetDetails(_testServer.Id, _testIdentity);
+
+            Assert.IsNotNull(serverDetails);
+            Assert.AreEqual("test-server", serverDetails.Name);
+            Assert.AreEqual("d531a2dd-7ae9-4407-bb5a-e5ea03303d98", serverDetails.Image.Id);
+            Assert.AreEqual("2", serverDetails.Flavor.Id);
+        }
+
+        [TestMethod]
+        public void Test3_Should_Wait_Until_Server_Becomes_Active_Or_A_Maximum_Of_10_Minutes()
+        {
+            var provider = new net.openstack.Providers.Rackspace.ComputeProvider();
+            var serverDetails = provider.GetDetails(_testServer.Id, _testIdentity);
+            Assert.IsNotNull(serverDetails);
+            
+            int count = 0;
+            while (!serverDetails.Status.Equals("ACTIVE") && !serverDetails.Status.Equals("ERROR") && !serverDetails.Status.Equals("UNKNOWN") && !serverDetails.Status.Equals("SUSPENDED") && count < 600)
+            {
+                Thread.Sleep(1000);
+                serverDetails = provider.GetDetails(_testServer.Id, _testIdentity);
+                count++;
+            }
+
+            Assert.IsNotNull(serverDetails);
+            Assert.AreEqual("ACTIVE", serverDetails.Status);
+        }
+
+        [TestMethod]
+        public void Test4_Should_Get_A_List_Of_Servers_Which_Should_Include_The_Newly_Created_Server()
+        {
+            var provider = new net.openstack.Providers.Rackspace.ComputeProvider();
+            var servers = provider.ListServers(_testIdentity);
+
+            Assert.IsNotNull(servers);
+            Assert.IsTrue(servers.Any());
+            Assert.IsNotNull(servers.FirstOrDefault(s => s.Id == _testServer.Id));
+        }
+
+        [TestMethod]
+        public void Test5_Should_Mark_The_Server_For_Deletion()
+        {
+            var provider = new net.openstack.Providers.Rackspace.ComputeProvider();
+            var result = provider.DeleteServer(_testServer.Id, _testIdentity);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test6_Should_Wait_A_Max_Of_10_Minutes_For_The_Server_Is_Deleted_Indicated_By_A_Null_Return_Value_For_Details()
+        {
+            var provider = new net.openstack.Providers.Rackspace.ComputeProvider();
+            var details = provider.GetDetails(_testServer.Id, _testIdentity);
+            Assert.IsNotNull(details);
+            
+            while (details != null && (!details.Status.Equals("DELETED") && !details.Status.Equals("ERROR") && !details.Status.Equals("UNKNOWN") && !details.Status.Equals("SUSPENDED")))
+            {
+                Thread.Sleep(1000);
+                details = provider.GetDetails(_testServer.Id, _testIdentity);
+            }
+
+            if(details != null)
+                Assert.AreEqual("DELETED", details.Status);
+        }
+    }
+}
