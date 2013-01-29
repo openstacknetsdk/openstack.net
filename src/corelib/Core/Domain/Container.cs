7ff4d226bd4be456fa5e9ffd25b11ea77dee6b6b
Ravi Kamachi <Ravi.Kamachi@rackspace.com>
2013-01-29 15:03:29 -0600

    Added support for ObjectStorage, starting with one method, List Containers, added integration tests. Removed hardcoded test settings.

---
+++ src/corelib/Core/Domain/Container.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace net.openstack.Core.Domain
+{
+    [DataContract]
+    public class Container
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int Count { get; set; }
+
+        [DataMember]
+        public long Bytes { get; set; }
+    }
+}
---
+++ src/corelib/Providers/Rackspace/ObjectStoreProvider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimpleRestServices.Client;
+using SimpleRestServices.Client.Json;
+using net.openstack.Core;
+using net.openstack.Core.Domain;
+using net.openstack.Providers.Rackspace.Objects.Request;
+using net.openstack.Providers.Rackspace.Objects.Response;
+
+namespace net.openstack.Providers.Rackspace
+{
+    public class ObjectStoreProvider : ProviderBase, IObjectStoreProvider
+    {
+        #region Constructors
+
+        public ObjectStoreProvider()
+            : this(new IdentityProvider(), new JsonRestServices()) { }
+
+        public ObjectStoreProvider(IIdentityProvider identityProvider, IRestService restService)
+            : base(identityProvider, restService) { }
+
+        #endregion
+
+        #region Containers
+
+        public IEnumerable<Container> ListContainers(CloudIdentity identity, int? limit = null, string marker = null, string markerEnd = null, string format = "json", string region = null)
+        {
+            var urlPath = new Uri(string.Format("{0}", GetServiceEndpoint(identity, region)));
+
+            var queryStringParameter = new Dictionary<string, string>();
+            queryStringParameter.Add("format", format);
+
+            if (limit != null)
+                queryStringParameter.Add("limit", limit.ToString());
+
+            if (!string.IsNullOrWhiteSpace(marker))
+                queryStringParameter.Add("marker", marker);
+
+            if (!string.IsNullOrWhiteSpace(markerEnd))
+                queryStringParameter.Add("end_marker", markerEnd);
+            
+            var response = ExecuteRESTRequest<Container []>(identity, urlPath, HttpMethod.GET, null, queryStringParameter);
+
+            if (response == null || response.Data == null)
+                return null;
+
+            return response.Data;
+
+        }
+
+        #endregion
+
+        #region Private methods
+
+        protected string GetServiceEndpoint(CloudIdentity identity, string region = null)
+        {
+            return base.GetServiceEndpoint(identity, "cloudFiles", region);
+        }
+
+        #endregion
+    }
+}
