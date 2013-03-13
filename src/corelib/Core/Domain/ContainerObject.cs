6db5f4cb1e3816b619503a7c6b6b34eb4f218272
Ravi Kamachi <Ravi.Kamachi@rackspace.com>
2013-03-12 23:25:23 -0500

    Get Objects from container functionality.

---
+++ src/corelib/Core/Domain/ContainerObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace net.openstack.Core.Domain
+{
+    [DataContract]
+    public class ContainerObject
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public Guid Hash { get; set; }
+
+        [DataMember]
+        public long Bytes { get; set; }
+
+        [DataMember(Name = "content_type")]
+        public string ContentType { get; set; }
+
+        [DataMember(Name = "last_modified")]
+        public DateTime LastModified { get; set; }
+    }
+}
