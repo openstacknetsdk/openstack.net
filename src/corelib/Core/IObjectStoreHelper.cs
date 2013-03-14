5c814ea32533361fe35694b20707203ea3fff3ad
Ravi Kamachi <Ravi.Kamachi@rackspace.com>
2013-03-13 22:34:40 -0500

    Object Headers functionality

---
+++ src/corelib/Core/IObjectStoreHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JSIStudios.SimpleRESTServices.Client;
+
+namespace net.openstack.Core
+{
+    public interface IObjectStoreHelper
+    {
+        void ValidateContainerName(string containerName);
+        void ValidateObjectName(string objectName);
+        Dictionary<string, Dictionary<string, string>> ProcessMetadata(IList<HttpHeader> httpHeaders);
+    }
+}
