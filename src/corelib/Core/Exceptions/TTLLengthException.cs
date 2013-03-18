f7243d48b834560215caefce8124e50ff3ee128f
unknown <kama4774@.RACKSPACE.CORP>
2013-03-18 16:39:52 -0500

    List CDN containers, make container CDN Enabled, make container CDN disabled

---
+++ src/corelib/Core/Exceptions/TTLLengthException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace net.openstack.Core.Exceptions
+{
+    public class TTLLengthException : Exception
+    {
+        public TTLLengthException() { }
+        public TTLLengthException(string message) : base(message) { }
+    }
+}
