28dded54cd94313e99326da48d283defedb30660
unknown <kama4774@.RACKSPACE.CORP>
2013-04-03 16:09:10 -0500

    [Delivers #43555149]Get object metadata functionality

--- src/corelib/Core/IObjectStoreProvider.cs
+++ src/corelib/Core/IObjectStoreProvider.cs
@@ -15,9 +15,9 @@ namespace net.openstack.Core
         IEnumerable<Container> ListContainers(CloudIdentity identity, int? limit = null, string markerId = null, string markerEnd = null, string format = "json", string region = null);
         ObjectStore CreateContainer(CloudIdentity identity, string container, string region = null);
         ObjectStore DeleteContainer(CloudIdentity identity, string container, string region = null);
-        Dictionary<string, string> GetHeaderForContainer(CloudIdentity identity, string container, string region = null, bool useInternalUrl = false);
-        Dictionary<string, string> GetMetaDataForContainer(CloudIdentity identity, string container, string region = null, bool useInternalUrl = false);
-        Dictionary<string, string> GetCDNHeaderForContainer(CloudIdentity identity, string container, string region = null, bool useInternalUrl = false);
+        Dictionary<string, string> GetContainerHeader(CloudIdentity identity, string container, string region = null, bool useInternalUrl = false);
+        Dictionary<string, string> GetContainerMetaData(CloudIdentity identity, string container, string region = null, bool useInternalUrl = false);
+        Dictionary<string, string> GetContainerCDNHeader(CloudIdentity identity, string container, string region = null, bool useInternalUrl = false);
 
         IEnumerable<ContainerCDN> ListCDNContainers(CloudIdentity identity, int? limit = null, string markerId = null, string markerEnd = null, bool cdnEnabled = false, string region = null);
 
@@ -37,7 +37,6 @@ namespace net.openstack.Core
         #region Container Objects
 
         IEnumerable<ContainerObject> GetObjects(CloudIdentity identity, string container, int? limit = null, string markerId = null, string markerEnd = null, string format = "json", string region = null);
-        IEnumerable<HttpHeader> GetObjectHeaders(CloudIdentity identity, string container, string objectName, string format = "json", string region = null);
         void CreateObjectFromFile(CloudIdentity identity, string container, string filePath, string objectName, int chunkSize = 65536, string region = null, Action<long> progressUpdated = null);
         void CreateObjectFromFile(CloudIdentity identity, string container, string filePath, string objectName, int chunkSize = 65536, Dictionary<string, string> headers = null, string region = null, Action<long> progressUpdated = null);
         void CreateObjectFromStream(CloudIdentity identity, string container, Stream stream, string objectName, int chunkSize = 65536, string region = null, Action<long> progressUpdated = null);
@@ -47,6 +46,8 @@ namespace net.openstack.Core
         ObjectStore DeleteObject(CloudIdentity identity, string container, string objectNmae, Dictionary<string, string> headers = null, string region = null);
         ObjectStore CopyObject(CloudIdentity identity, string sourceContainer, string sourceObjectName, string destinationContainer, string destinationObjectName, Dictionary<string, string> headers = null, string region = null);
 
+        Dictionary<string, string> GetObjectHeaders(CloudIdentity identity, string container, string objectName, string format = "json", string region = null);
+        Dictionary<string, string> GetObjectMetaData(string container, string objectName, string region = null, bool useInternalUrl = false, CloudIdentity identity = null);
         #endregion
 
         //string Name { get; }
