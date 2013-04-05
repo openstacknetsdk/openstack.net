e5c3b7d3754338b25749eabf6bbff2e9c78eaab7
Jason Mitschke <jason.mitschke@rackspace.com>
2013-04-05 16:25:05 -0500

    [Delivers #43560237 #43560235] Add Show Volume List and Show Volume Methods

---
+++ src/corelib/Core/Domain/Volume.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace net.openstack.Core.Domain
+{
+    [DataContract]
+    public class Volume
+    {
+        [DataMember]
+        public string Id { get; set; }
+
+        [DataMember(Name = "display_name")]
+        public string DisplayName { get; set; }
+
+        [DataMember(Name = "display_description")]
+        public string DisplayDescription { get; set; }
+
+        [DataMember]
+        public int Size { get; set; }
+
+        [DataMember(Name = "volume_type")]
+        public string VolumeType { get; set; }
+
+        [DataMember(Name = "snapshot_id")]
+        public string SnapshotId { get; set; }
+
+        [DataMember]
+        public string[] Attachments { get; set; }
+
+        [DataMember]
+        public string Status { get; set; }
+
+        [DataMember(Name = "availability_zone")]
+        public string AvailabilityZone { get; set; }
+
+        [DataMember(Name = "created_at")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
