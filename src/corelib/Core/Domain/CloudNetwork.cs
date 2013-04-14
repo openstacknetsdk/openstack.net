47c5a90268b8cc44b3fd43e37a2676b0b3d38419
Bret McGowen <bret.mcgowen@rackspace.com>
2013-04-14 01:53:26 -0500

    Added Cloud Network support

---
+++ src/corelib/Core/Domain/CloudNetwork.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace net.openstack.Core.Domain
+{
+    [DataContract]
+    public class CloudNetwork
+    {
+        /// <summary>
+        /// The network ID.
+        /// </summary>
+        [DataMember]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The CIDR for an isolated network.
+        /// </summary>
+        [DataMember]
+        public string Cidr { get; set; }
+
+        /// <summary>
+        /// The name of the network. ServiceNet is labeled as private and PublicNet 
+        /// is labeled as public in the network list.
+        /// </summary>
+        [DataMember]
+        public string Label { get; set; }
+    }
+}
