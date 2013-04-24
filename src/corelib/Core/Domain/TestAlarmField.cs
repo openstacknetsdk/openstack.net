d3a6a7686efc4b7a4493f0895d5d1d1ee1f26e16
Danny A. Pallone II <dan.pallone@rackspace.com>
2013-04-24 10:36:48 -0500

    Account Get Limits functionality, test coverage

---
+++ src/corelib/Core/Domain/TestAlarmField.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace net.openstack.Core.Domain
+{
+    /// <summary>
+    /// <param name="limit">Maximum tolerable limit before alarm occurs.</param>
+    /// <param name="used">How many times an instance happened within a given window.</param>
+    /// <param name="window">Time window for limits.</param>
+    /// </summary>
+    [DataContract]
+    public class TestAlarmField
+    {
+
+        [DataMember(Name = "limit")]
+        public int Limit { get; set; }
+
+        [DataMember(Name = "used")]
+        public int Used { get; set; }
+
+        [DataMember(Name = "window")]
+        public string Window { get; set; }
+    }
+}
