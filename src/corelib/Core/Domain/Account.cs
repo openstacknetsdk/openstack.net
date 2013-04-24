89ec0e489da792b527a57bc72bc59c1202a05553
Danny A. Pallone II <dan.pallone@rackspace.com>
2013-04-24 14:19:04 -0500

    Get Account functionality, tests coverage

---
+++ src/corelib/Core/Domain/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace net.openstack.Core.Domain
+{
+	[DataContract]
+    public class Account
+    {
+
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "metadata")]
+        public AccountMetaData Metadata { get; set; }
+
+        [DataMember(Name = "webhook_token")]
+        public string WebhookToken { get; set; }
+    }
+}
