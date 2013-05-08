809882ab5b8ee96acc8c6f61e0b4949a046651be
Alan Quillin <alan.quillin@rackspace.com>
2013-05-08 12:34:29 -0500

    Added initial Provider.  Added List and Get by ID methods.

---
+++ src/corelib/Providers/Rackspace/Objects/Request/CreateLoadBalancerRequest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using net.openstack.Core.Domain;
+
+namespace net.openstack.Providers.Rackspace.Objects.Request
+{
+    [DataContract]
+    internal class CreateLoadBalancerRequest
+    {
+        [DataMember(Name = "loadBalancer")]
+        public NewLoadBalancer LoadBalancer { get; set; }
+    }
+
+    [DataContract]
+    internal class NewLoadBalancer
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        //[DataMember(Name = "nodes")]
+
+        [DataMember(Name = "protocol")]
+        public string Protocol { get; set; }
+
+        [DataMember(Name = "virtualIps")]
+        public IEnumerable<NewVirtualIP> VirtualIPs { get; set; }
+
+        [DataMember(Name = "port")]
+        public int Port { get; set; }
+
+        [DataMember(Name = "halfClosed", EmitDefaultValue = false)]
+        public bool HalfClosed { get; set; }
+
+        //[DataMember(Name = "accessList", EmitDefaultValue = false)]
+
+        //[DataMember(Name = "algorithm", EmitDefaultValue = false)]
+        //public string Algorithm { get; set; }
+
+        //[DataMember(Name = "connectionLogging", EmitDefaultValue = false)]
+
+        //[DataMember(Name = "connectionThrottle", EmitDefaultValue = false)]
+
+        //[DataMember(Name = "healthMonitor", EmitDefaultValue = false)]
+
+        //[DataMember(Name = "metadata", EmitDefaultValue = false)]
+
+        //[DataMember(Name = "timeout", EmitDefaultValue = false)]
+        //public int Timeout { get; set; }
+
+        //[DataMember(Name = "sessionPersistence", EmitDefaultValue = false)]        
+    }
+
+    [DataContract]
+    internal class NewVirtualIP
+    {
+        [DataMember(Name = "type")]
+        public string Type { get; set; }
+    }
+}
---
+++ src/corelib/Providers/Rackspace/Objects/Response/ListLoadBalancersResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using net.openstack.Core.Domain;
+
+namespace net.openstack.Providers.Rackspace.Objects.Response
+{
+    [DataContract]
+    internal class ListLoadBalancersResponse
+    {
+        [DataMember(Name = "loadBalancers")]
+        public IEnumerable<SimpleLoadBalancer> LoadBalancers { get; set; }
+    }
+}
