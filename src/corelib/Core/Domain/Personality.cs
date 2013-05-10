bab20c93743d2bece21b7694f9eb3473ca2799e0
dotnetpro <monty@dotnetpro.co.uk>
2013-05-10 14:56:31 +0100

    Updated Personality to be array, tested on live
    Updated Personality to be array, tested on live, tested on Live
    Rackspace API.

--- src/corelib/Core/Domain/Personality.cs
+++ src/corelib/Core/Domain/Personality.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace net.openstack.Core.Domain
 {
+    [DataContract]
     public class Personality
     {
+        [DataMember(Name = "path")]
         public string Path { get; set; }
 
+        [DataMember(Name = "contents")]
         public string Content { get; set; }
     }
 }
