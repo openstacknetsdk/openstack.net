61b4c7063bb8e29154a8078a53077e6e308c1f31
Sam Harwell <sam.harwell@rackspace.com>
2013-08-02 15:06:32 -0500

    Updated documentation for exception classes

--- src/corelib/Core/Exceptions/InvalidCloudIdentityException.cs
+++ src/corelib/Core/Exceptions/InvalidCloudIdentityException.cs
@@ -1,13 +1,33 @@
 using System;
 using System.Runtime.Serialization;
+using net.openstack.Core.Domain;
 
 namespace net.openstack.Core.Exceptions
 {
+    /// <summary>
+    /// The exception thrown when the <see cref="CloudIdentity"/> instance passed
+    /// to a provider method is not supported by that provider.
+    /// </summary>
     [Serializable]
     internal class InvalidCloudIdentityException : Exception
     {
-        public InvalidCloudIdentityException(string message) : base(message) {}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCloudIdentityException"/> class
+        /// with the specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public InvalidCloudIdentityException(string message)
+            : base(message)
+        {
+        }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCloudIdentityException"/> class with
+        /// serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="info"/> is <c>null</c>.</exception>
         protected InvalidCloudIdentityException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
