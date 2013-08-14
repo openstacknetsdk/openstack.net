19d49967195d6b676a67da537e3425b4f873f2af
Sam Harwell <sam.harwell@rackspace.com>
2013-08-14 14:48:11 -0500

    Use the PhysicalAddress class to represent a MAC address (fixes #137)

--- src/corelib/Core/Domain/VirtualInterface.cs
+++ src/corelib/Core/Domain/VirtualInterface.cs
@@ -1,17 +1,29 @@
 ﻿namespace net.openstack.Core.Domain
 {
+    using System.Net.NetworkInformation;
     using Newtonsoft.Json;
 
     [JsonObject(MemberSerialization.OptIn)]
     public class VirtualInterface
     {
+        [JsonProperty("mac_address")]
+        private string _macAddress;
+
         [JsonProperty("id")]
         public string Id { get; private set; }
 
         [JsonProperty("ip_addresses")]
         public VirtualInterfaceAddress[] Addresses { get; private set; }
 
-        [JsonProperty("mac_address")]
-        public string MACAddress { get; private set; }
+        public PhysicalAddress MACAddress
+        {
+            get
+            {
+                if (_macAddress == null)
+                    return null;
+
+                return PhysicalAddress.Parse(_macAddress);
+            }
+        }
     }
 }
