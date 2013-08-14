b468f9a76898aa9cb2b8a9966035e151d9793572
Sam Harwell <sam.harwell@rackspace.com>
2013-08-14 10:54:53 -0500

    Updated documentation for ServiceCatalog

--- src/corelib/Core/Domain/ServiceCatalog.cs
+++ src/corelib/Core/Domain/ServiceCatalog.cs
@@ -2,15 +2,34 @@ namespace net.openstack.Core.Domain
 {
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Represents a single service provided to an authenticated user. Each service
+    /// has one or more <see cref="Endpoints"/> providing access information for the
+    /// service.
+    /// </summary>
+    /// <seealso href="http://docs.openstack.org/api/openstack-identity-service/2.0/content/POST_authenticate_v2.0_tokens_.html">Authenticate (OpenStack Identity Service API v2.0 Reference)</seealso>
     [JsonObject(MemberSerialization.OptIn)]
     public class ServiceCatalog
     {
+        /// <summary>
+        /// Gets the endpoints for the service.
+        /// </summary>
+        /// <seealso href="http://docs.openstack.org/api/openstack-identity-service/2.0/content/POST_authenticate_v2.0_tokens_.html">Authenticate (OpenStack Identity Service API v2.0 Reference)</seealso>
         [JsonProperty("endpoints")]
         public Endpoint[] Endpoints { get; private set; }
 
+        /// <summary>
+        /// Gets the display name of the service, which may be a vendor-specific
+        /// product name.
+        /// </summary>
+        /// <seealso href="http://docs.openstack.org/api/openstack-identity-service/2.0/content/POST_authenticate_v2.0_tokens_.html">Authenticate (OpenStack Identity Service API v2.0 Reference)</seealso>
         [JsonProperty("name")]
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Gets the canonical name of the specification implemented by this service.
+        /// </summary>
+        /// <seealso href="http://docs.openstack.org/api/openstack-identity-service/2.0/content/POST_authenticate_v2.0_tokens_.html">Authenticate (OpenStack Identity Service API v2.0 Reference)</seealso>
         [JsonProperty("type")]
         public string Type { get; private set; }
     }
