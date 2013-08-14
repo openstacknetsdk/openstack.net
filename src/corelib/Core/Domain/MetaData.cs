40ef3b9a416e947190eacf2b14045a7ba8096b2e
Sam Harwell <sam.harwell@rackspace.com>
2013-08-14 14:51:25 -0500

    Document Metadata, Role, VirtualInterface, UserAgentGenerator

--- src/corelib/Core/Domain/MetaData.cs
+++ src/corelib/Core/Domain/MetaData.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 
 namespace net.openstack.Core.Domain
 {
+    /// <summary>
+    /// Represents metadata for servers and images in the Compute Service.
+    /// </summary>
+    /// <remarks>
+    /// The metadata keys for the compute provider are case-sensitive.
+    /// </remarks>
     [Serializable]
     public class Metadata : Dictionary<string, string>
     {
--- src/corelib/Core/Domain/VirtualInterface.cs
+++ src/corelib/Core/Domain/VirtualInterface.cs
@@ -3,18 +3,33 @@
     using System.Net.NetworkInformation;
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Represents the detailed configuration of a virtual network interface.
+    /// </summary>
     [JsonObject(MemberSerialization.OptIn)]
     public class VirtualInterface
     {
         [JsonProperty("mac_address")]
         private string _macAddress;
 
+        /// <summary>
+        /// Gets the virtual interface ID.
+        /// </summary>
+        /// <seealso href="http://docs.rackspace.com/networks/api/v2/cn-devguide/content/list_virt_interfaces.html">List Virtual Interfaces (Rackspace Cloud Networks Developer Guide - OpenStack Networking API v2)</seealso>
         [JsonProperty("id")]
         public string Id { get; private set; }
 
+        /// <summary>
+        /// Gets the network addresses associated with the virtual interface.
+        /// </summary>
+        /// <seealso href="http://docs.rackspace.com/networks/api/v2/cn-devguide/content/list_virt_interfaces.html">List Virtual Interfaces (Rackspace Cloud Networks Developer Guide - OpenStack Networking API v2)</seealso>
         [JsonProperty("ip_addresses")]
         public VirtualInterfaceAddress[] Addresses { get; private set; }
 
+        /// <summary>
+        /// Gets the Media Access Control (MAC) address for the virtual interface.
+        /// </summary>
+        /// <seealso href="http://docs.rackspace.com/networks/api/v2/cn-devguide/content/list_virt_interfaces.html">List Virtual Interfaces (Rackspace Cloud Networks Developer Guide - OpenStack Networking API v2)</seealso>
         public PhysicalAddress MACAddress
         {
             get
