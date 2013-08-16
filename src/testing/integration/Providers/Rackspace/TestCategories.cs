83c14812394b103d6bdd6979c7950e0ae2660ff6
Sam Harwell <sam@tunnelvisionlabs.com>
2013-08-15 23:38:32 -0500

    Created a set of tests for IIdentityProvider that can be run with user credentials only (fixes #156)

---
+++ src/testing/integration/Providers/Rackspace/TestCategories.cs
@@ -0,0 +1,23 @@
+﻿namespace Net.OpenStack.Testing.Integration.Providers.Rackspace
+{
+    /// <summary>
+    /// Provides predefined categories for use with the <see cref="TestCategoryAttribute"/>.
+    /// </summary>
+    public static class TestCategories
+    {
+        /// <summary>
+        /// Identity service tests.
+        /// </summary>
+        public const string Identity = "Identity";
+
+        /// <summary>
+        /// Tests which require user credentials.
+        /// </summary>
+        public const string User = "User";
+
+        /// <summary>
+        /// Tests which require administrator credentials.
+        /// </summary>
+        public const string Admin = "Admin";
+    }
+}
