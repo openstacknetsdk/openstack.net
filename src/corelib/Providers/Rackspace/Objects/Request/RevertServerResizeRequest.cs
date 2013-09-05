bf039f131e53a84e0faf64f6fb8f877263bf7cd2
Sam Harwell <sam.harwell@rackspace.com>
2013-09-05 10:03:30 -0500

    Updated documentation for RevertServerResizeRequest

--- src/corelib/Providers/Rackspace/Objects/Request/RevertServerResizeRequest.cs
+++ src/corelib/Providers/Rackspace/Objects/Request/RevertServerResizeRequest.cs
@@ -2,15 +2,16 @@ namespace net.openstack.Providers.Rackspace.Objects.Request
 {
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// This models the JSON request used for the Revert Resized Server request.
+    /// </summary>
+    /// <seealso href="http://docs.openstack.org/api/openstack-compute/2/content/Revert_Resized_Server-d1e4024.html">Revert Resized Server (OpenStack Compute API v2 and Extensions Reference)</seealso>
     [JsonObject(MemberSerialization.OptIn)]
     internal class RevertServerResizeRequest
     {
-        [JsonProperty("revertResize")]
-        public string Details { get; set; }
-
-        public RevertServerResizeRequest()
-        {
-            Details = string.Empty;
-        }
+#pragma warning disable 169 // The field 'fieldName' is never used
+        [JsonProperty("revertResize", DefaultValueHandling = DefaultValueHandling.Include)]
+        private string _command;
+#pragma warning restore 169
     }
 }
