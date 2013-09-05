1652ec21009d5062aafd5214db1f19457c68ca48
Sam Harwell <sam.harwell@rackspace.com>
2013-09-05 08:37:33 -0500

    Updated documentation for ConfirmServerResizeRequest

--- src/corelib/Providers/Rackspace/Objects/Request/ConfirmServerResizeRequest.cs
+++ src/corelib/Providers/Rackspace/Objects/Request/ConfirmServerResizeRequest.cs
@@ -2,15 +2,16 @@ namespace net.openstack.Providers.Rackspace.Objects.Request
 {
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// This models the JSON request used for the Confirm Resized Server request.
+    /// </summary>
+    /// <seealso href="http://docs.openstack.org/api/openstack-compute/2/content/Confirm_Resized_Server-d1e3868.html">Confirm Resized Server (OpenStack Compute API v2 and Extensions Reference)</seealso>
     [JsonObject(MemberSerialization.OptIn)]
     internal class ConfirmServerResizeRequest
     {
-        [JsonProperty("confirmResize")]
-        public string Details { get; set; }
-
-        public ConfirmServerResizeRequest()
-        {
-            Details = string.Empty;
-        }
+#pragma warning disable 169 // The field 'fieldName' is never used
+        [JsonProperty("confirmResize", DefaultValueHandling = DefaultValueHandling.Include)]
+        private string _command;
+#pragma warning restore 169
     }
 }
