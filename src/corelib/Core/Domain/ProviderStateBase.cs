6a16ead243c375b08e9256e08b8d3430deccf61e
Sam Harwell <sam.harwell@rackspace.com>
2013-09-10 12:40:42 -0500

    Document ProviderStateBase<T> and provide protected access to its properties

--- src/corelib/Core/Domain/ProviderStateBase.cs
+++ src/corelib/Core/Domain/ProviderStateBase.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace net.openstack.Core.Domain
+﻿namespace net.openstack.Core.Domain
 {
+    /// <summary>
+    /// Provides a base class for domain objects which require access to the
+    /// provider which created them.
+    /// </summary>
+    /// <typeparam name="T">The provider type.</typeparam>
     public abstract class ProviderStateBase<T>
     {
-        internal T Provider { get; set; }
+        /// <summary>
+        /// Gets the provider which created the instance.
+        /// </summary>
+        protected internal T Provider { get; set; }
 
-        internal string Region { get; set; }
+        /// <summary>
+        /// Gets the region where the current entity resides.
+        /// </summary>
+        protected internal string Region { get; set; }
 
-        internal CloudIdentity Identity { get; set; }
+        /// <summary>
+        /// Gets the identity the current entity belongs to, or <c>null</c> if
+        /// the identity was not explicitly specified in the request that created
+        /// this object (i.e. the default identity of <see cref="Provider"/> was
+        /// used).
+        /// </summary>
+        protected internal CloudIdentity Identity { get; set; }
     }
 }
