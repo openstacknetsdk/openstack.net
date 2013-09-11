f39e385895415002614584eb54a35039e5f7761e
Sam Harwell <sam.harwell@rackspace.com>
2013-09-11 16:45:03 -0500

    Updated documentation for domain objects

--- src/corelib/Core/Domain/NewUser.cs
+++ src/corelib/Core/Domain/NewUser.cs
@@ -1,25 +1,62 @@
 ﻿namespace net.openstack.Core.Domain
 {
+    using net.openstack.Core.Providers;
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Represents the JSON result of an Add User operation.
+    /// </summary>
+    /// <seealso cref="IIdentityProvider.AddUser"/>
+    /// <seealso href="http://docs.openstack.org/api/openstack-identity-service/2.0/content/POST_addUser_v2.0_users_.html">Add User (OpenStack Identity Service API v2.0 Reference)</seealso>
+    /// <seealso href="http://docs.rackspace.com/auth/api/v2.0/auth-client-devguide/content/POST_addUser_v2.0_users_.html">Add User (Rackspace Cloud Identity Client Developer Guide - API v2.0)</seealso>
     [JsonObject(MemberSerialization.OptIn)]
     public class NewUser
     {
+        /// <summary>
+        /// Gets the generated password for the new user.
+        /// <note type="warning">The value of this property is not defined by OpenStack, and may not be consistent across vendors.</note>
+        /// </summary>
+        /// <value>The generated password for the new user, or <c>null</c> if the Add User request included a password.</value>
         [JsonProperty("OS-KSADM:password")]
         public string Password { get; internal set; }
 
+        /// <summary>
+        /// Gets the ID for the new user.
+        /// <note type="warning">The value of this property is not defined. Do not use.</note>
+        /// </summary>
         [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Include)]
         public string Id { get; private set; }
 
+        /// <summary>
+        /// Gets the username of the new user.
+        /// <note type="warning">The value of this property is not defined. Do not use.</note>
+        /// </summary>
         [JsonProperty("username")]
         public string Username { get; private set; }
 
+        /// <summary>
+        /// Gets the email address of the new user.
+        /// <note type="warning">The value of this property is not defined. Do not use.</note>
+        /// </summary>
         [JsonProperty("email")]
         public string Email { get; private set; }
 
+        /// <summary>
+        /// Gets a value indicating whether or not the user is enabled.
+        /// <note type="warning">The value of this property is not defined. Do not use.</note>
+        /// </summary>
         [JsonProperty("enabled")]
         public bool Enabled { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NewUser"/> class with the specified
+        /// username, email address, password, and value indicating whether or not the user
+        /// is initially enabled.
+        /// </summary>
+        /// <param name="username">The username of the new user (see <see cref="Username"/>).</param>
+        /// <param name="email">The email address of the new user (see <see cref="Email"/>).</param>
+        /// <param name="password">The password for the new user (see <see cref="Password"/>).</param>
+        /// <param name="enabled"><c>true</c> if the user is initially enabled; otherwise, <c>false</c> (see <see cref="Enabled"/>).</param>
         public NewUser(string username, string email, string password = null, bool enabled = true)
         {
             Username = username;
--- src/corelib/Core/Domain/User.cs
+++ src/corelib/Core/Domain/User.cs
@@ -1,22 +1,67 @@
 namespace net.openstack.Core.Domain
 {
+    using net.openstack.Core.Providers;
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Represents a user account.
+    /// </summary>
+    /// <seealso cref="IIdentityProvider.ListUsers"/>
     [JsonObject(MemberSerialization.OptIn)]
     public class User
     {
+        /// <summary>
+        /// Gets or sets the default region of the user.
+        /// <note type="warning">The value of this property is not defined. Do not use.</note>
+        /// </summary>
+        /// <remarks>
+        /// Changes to this property are not automatically saved on the server. To apply the
+        /// changes, call <see cref="IIdentityProvider.UpdateUser"/> after setting this property.
+        ///
+        /// <note>
+        /// This property is a Rackspace-specific extension to the OpenStack Identity Service.
+        /// </note>
+        /// </remarks>
         [JsonProperty("RAX-AUTH:defaultRegion")]
         public string DefaultRegion { get; set; }
 
+        /// <summary>
+        /// Gets the unique identifier for the user.
+        /// <note type="warning">The value of this property is not defined. Do not use.</note>
+        /// </summary>
         [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Include)]
         public string Id { get; private set; }
 
+        /// <summary>
+        /// Gets or sets the "username" property of the user.
+        /// <note type="warning">The value of this property is not defined. Do not use.</note>
+        /// </summary>
+        /// <remarks>
+        /// Changes to this property are not automatically saved on the server. To apply the
+        /// changes, call <see cref="IIdentityProvider.UpdateUser"/> after setting this property.
+        /// </remarks>
         [JsonProperty("username")]
         public string Username { get; set; }
 
+        /// <summary>
+        /// Gets or sets the "email" property of the user.
+        /// <note type="warning">The value of this property is not defined. Do not use.</note>
+        /// </summary>
+        /// <remarks>
+        /// Changes to this property are not automatically saved on the server. To apply the
+        /// changes, call <see cref="IIdentityProvider.UpdateUser"/> after setting this property.
+        /// </remarks>
         [JsonProperty("email")]
         public string Email { get; set; }
 
+        /// <summary>
+        /// Gets or sets the "enabled" property of the user.
+        /// <note type="warning">The value of this property is not defined. Do not use.</note>
+        /// </summary>
+        /// <remarks>
+        /// Changes to this property are not automatically saved on the server. To apply the
+        /// changes, call <see cref="IIdentityProvider.UpdateUser"/> after setting this property.
+        /// </remarks>
         [JsonProperty("enabled")]
         public bool Enabled { get; set; }
     }
