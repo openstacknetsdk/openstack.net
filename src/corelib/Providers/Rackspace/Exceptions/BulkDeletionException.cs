38ff6382073a7a95a0093679eff31f4c9dfb9a2a
Sam Harwell <sam.harwell@rackspace.com>
2013-09-11 15:47:23 -0500

    Updated documentation for Rackspace exceptions

--- src/corelib/Providers/Rackspace/Exceptions/BulkDeletionException.cs
+++ src/corelib/Providers/Rackspace/Exceptions/BulkDeletionException.cs
@@ -3,13 +3,41 @@ using net.openstack.Providers.Rackspace.Objects;
 
 namespace net.openstack.Providers.Rackspace.Exceptions
 {
+    /// <summary>
+    /// Represents errors which occur during a bulk delete operation.
+    /// </summary>
+    /// <seealso cref="CloudFilesProvider.BulkDelete"/>
     public class BulkDeletionException : Exception
     {
+        /// <summary>
+        /// Gets the detailed results of the bulk delete operation.
+        /// </summary>
         public BulkDeletionResults Results { get; private set; }
-        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BulkDeletionException"/> class
+        /// with the specified status and results.
+        /// </summary>
+        /// <param name="status">A description of the status of the operation.</param>
+        /// <param name="results">The results of the bulk delete operation.</param>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="status"/> is <c>null</c>.
+        /// <para>-or-</para>
+        /// <para>If <paramref name="results"/> is <c>null</c>.</para>
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If <paramref name="status"/> is empty.
+        /// </exception>
         public BulkDeletionException(string status, BulkDeletionResults results)
-            : base(string.Format("The bulk deletion operation did not complete successfully. Status code: {0}", status))
+            : base(string.Format("The bulk deletion operation did not complete successfully. Status: {0}", status))
         {
+            if (status == null)
+                throw new ArgumentNullException("status");
+            if (results == null)
+                throw new ArgumentNullException("results");
+            if (string.IsNullOrEmpty(status))
+                throw new ArgumentException("status cannot be empty");
+
             Results = results;
         }
     }
