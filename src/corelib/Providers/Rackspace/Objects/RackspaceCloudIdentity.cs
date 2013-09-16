d8625c2ff3d91138c44e516612fb3e079fc04be0
Sam Harwell <sam.harwell@rackspace.com>
2013-09-16 10:10:58 -0500

    Only 1 top-level type per file

--- src/corelib/Providers/Rackspace/Objects/RackspaceCloudIdentity.cs
+++ src/corelib/Providers/Rackspace/Objects/RackspaceCloudIdentity.cs
@@ -59,25 +59,4 @@ namespace net.openstack.Providers.Rackspace.Objects
         /// </remarks>
         public Domain Domain { get; set; }
     }
-
-    /// <summary>
-    /// Represents a particular Rackspace entity where a user's account is located.
-    /// </summary>
-    public enum CloudInstance
-    {
-        /// <summary>
-        /// The Rackspace cloud for US-based accounts.
-        /// </summary>
-        US,
-
-        /// <summary>
-        /// The Rackspace cloud for UK-based accounts.
-        /// </summary>
-        UK,
-
-        /// <summary>
-        /// The default Rackspace cloud, which is currently equal to <see cref="US"/>.
-        /// </summary>
-        Default = US,
-    }
 }
