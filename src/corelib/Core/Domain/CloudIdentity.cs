d8ae187256f1cd9e67d575dd19ac78968a0e029f
osenft <osk@senft-net.com>
2013-09-18 19:16:59 -0400

    Extend CloudIdentity with ProjectName property
    The open source version of OpenStack requires the ProjectName parameter
    to be included in the AuthRequest. Adding it here to allow communication
    with OpenStack.

--- src/corelib/Core/Domain/CloudIdentity.cs
+++ src/corelib/Core/Domain/CloudIdentity.cs
@@ -42,5 +42,19 @@
         /// should be performed by different method (e.g. with a <see cref="Password"/>).
         /// </value>
         public string APIKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the project (tenant) name for this identity.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="CloudIdentity"/> class represents <em>credentials</em> (as opposed
+        /// to an <em>account</em>), so any changes made to this property value will not be
+        /// reflected in the account.
+        /// </remarks>
+        /// <value>
+        /// The name of the project for this identity. The value may be <c>null</c> if the particular
+        /// provider supports authenticating without a project.
+        /// </value>
+        public string ProjectName { get; set; }
     }
 }
