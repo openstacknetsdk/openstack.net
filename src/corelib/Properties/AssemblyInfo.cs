18e77253f0053c910ae90b6875d785b8237e3a26
Sam Harwell <sam.harwell@rackspace.com>
2013-10-31 09:52:22 -0500

    Update assembly info and dependencies (fixes #213)

--- src/corelib/Properties/AssemblyInfo.cs
+++ src/corelib/Properties/AssemblyInfo.cs
@@ -22,18 +22,11 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("22d9e69a-659a-4699-826f-18ec39657748")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.1")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
+// Refer to the following issue before changing these version numbers:
+// https://github.com/JSIStudios/SimpleRestServices/issues/53
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0.0-dev")]
 
 [assembly: InternalsVisibleTo("OpenStackNet.Testing.Integration")]
 [assembly: InternalsVisibleTo("OpenStackNet.Testing.Unit")]
