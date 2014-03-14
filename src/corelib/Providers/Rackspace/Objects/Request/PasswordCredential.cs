ed9fad5fd217dd77e594e5e98923a8e7108aeb7c
Sam Harwell <sam.harwell@rackspace.com>
2014-03-14 11:18:25 -0500

    Remove unresolvable reference to internal classes from public API documentation

--- src/corelib/Providers/Rackspace/Objects/Request/PasswordCredential.cs
+++ src/corelib/Providers/Rackspace/Objects/Request/PasswordCredential.cs
@@ -7,8 +7,6 @@
     /// <summary>
     /// This models the JSON representation of password credentials.
     /// </summary>
-    /// <seealso cref="SetPasswordRequest"/>
-    /// <seealso cref="PasswordCredentialResponse"/>
     /// <seealso href="http://docs.openstack.org/api/openstack-identity-service/2.0/content/POST_updateUserCredential_v2.0_users__userId__OS-KSADM_credentials__credential-type__.html">Update User Credentials (OpenStack Identity Service API v2.0 Reference)</seealso>
     /// <threadsafety static="true" instance="false"/>
     [JsonObject(MemberSerialization.OptIn)]
