44700a2f53b26b7ee4ac2ff18580c246d128aec8
Sam Harwell <sam.harwell@rackspace.com>
2014-08-19 07:08:47 -0500

    Remove reference to System.Web that was only used by a documentation comment

--- src/corelib/Core/UriPart.cs
+++ src/corelib/Core/UriPart.cs
@@ -1,8 +1,5 @@
 ﻿namespace net.openstack.Core
 {
-    extern alias web;
-    using HttpUtility = web.System.Web.HttpUtility;
-
     /// <summary>
     /// Represents a specific part of a URI.
     /// </summary>
@@ -18,7 +15,7 @@
         /// Represents a non-specific URL part, where all characters except unreserved characters and the characters <c>(</c>, <c>)</c>, <c>!</c>, and <c>*</c> are percent-encoded.
         /// </summary>
         /// <remarks>
-        /// When used with <see cref="UriUtility.UriEncode(string, UriPart)"/>, this URI part matches the behavior of <see cref="HttpUtility.UrlEncode(string)"/>, with the exception of space characters (this method percent-encodes space characters as <c>%20</c>).
+        /// When used with <see cref="UriUtility.UriEncode(string, UriPart)"/>, this URI part matches the behavior of <see cref="M:System.Web.HttpUtility.UrlEncode(string)"/>, with the exception of space characters (this method percent-encodes space characters as <c>%20</c>).
         /// </remarks>
         AnyUrl,
 
