9ce00838763a13284536857e0a11f786dbc1957a
Sam Harwell <sam.harwell@rackspace.com>
2014-08-20 12:56:51 -0500

    Refactor classes in the OpenStack.ObjectModel namespace

---
+++ src/OpenStack.Net/OpenStack/ObjectModel/JsonHome/NamespaceDoc.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenStack.ObjectModel.JsonHome
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// The <see cref="OpenStack.ObjectModel.JsonHome"/> namespace contains interfaces and
+    /// classes that define the object model for JSON Home Documents.
+    /// </summary>
+    /// <seealso cref="HomeDocument"/>
+    /// <seealso href="http://tools.ietf.org/html/draft-nottingham-json-home-03#section-2">JSON Home Documents (Home Documents for HTTP APIs - draft-nottingham-json-home-03)</seealso>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
