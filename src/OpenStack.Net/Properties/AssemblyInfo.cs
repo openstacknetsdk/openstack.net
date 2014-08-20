b99d12f8246e18470900fe57f3587d705bab7457
Sam Harwell <sam.harwell@rackspace.com>
2014-08-20 08:12:27 -0500

    Add new empty projects for the V2 SDK

---
+++ src/OpenStack.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+#if !PORTABLE
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+#endif
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenStack.Net")]
+[assembly: AssemblyDescription("OpenStack SDK for Microsoft .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rackspace US")]
+[assembly: AssemblyProduct("OpenStack.Net")]
+[assembly: AssemblyCopyright("Copyright © Rackspace US 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(false)]
+
+#if !PORTABLE
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d314814e-2f49-4c38-854b-c083cee1b85d")]
+#endif
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0-dev")]
