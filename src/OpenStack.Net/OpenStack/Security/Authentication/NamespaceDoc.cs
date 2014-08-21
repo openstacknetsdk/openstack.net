9dfc3f356b5cfca1de2f27e355bdd4b2bc559e5a
Sam Harwell <sam.harwell@rackspace.com>
2014-08-21 14:00:15 -0500

    Add the IAuthenticationService interface

---
+++ src/OpenStack.Net/OpenStack/Security/Authentication/NamespaceDoc.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenStack.Security.Authentication
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// The <see cref="OpenStack.Security.Authentication"/> namespace contains interfaces and classes that support
+    /// OpenStack service endpoint location and HTTP API request authentication.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
