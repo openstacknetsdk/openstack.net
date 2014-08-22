04592e86d672f81a515f32c3ce66c8ea03ac00f2
Sam Harwell <sam.harwell@rackspace.com>
2014-08-21 20:05:59 -0500

    Add the base classes for the OpenStack.Services client implementations

---
+++ src/OpenStack.Net/OpenStack/Services/NamespaceDoc.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenStack.Services
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// The <see cref="OpenStack.Services"/> namespaces contain interfaces and classes that define the OpenStack
+    /// services, extensions, and object models, as well as the default service client implementations for these
+    /// services.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
