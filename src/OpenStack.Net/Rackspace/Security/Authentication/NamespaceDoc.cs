f30134f2f2590e2646599e0ca0865132f28011a6
Sam Harwell <sam.harwell@rackspace.com>
2014-08-25 09:28:06 -0500

    Simplify the creation of Rackspace authentication requests

---
+++ src/OpenStack.Net/Rackspace/Security/Authentication/NamespaceDoc.cs
@@ -0,0 +1,13 @@
+﻿namespace Rackspace.Security.Authentication
+{
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// The <see cref="Rackspace.Security.Authentication"/> namespace contains interfaces and classes to support
+    /// authenticating HTTP API calls using the Rackspace Cloud Identity product.
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
