8cfc24202d44a0057da21ec438fef650f3155cc0
Sam Harwell <sam.harwell@rackspace.com>
2014-08-26 08:47:08 -0500

    Add WithQueryParameter extension methods for IHttpApiRequest

--- src/OpenStack.Net/OpenStack/Net/HttpApiRequestExtensions.cs
+++ src/OpenStack.Net/OpenStack/Net/HttpApiRequestExtensions.cs
@@ -69,5 +69,87 @@
 
             return apiRequestTask.Select(task => task.Result.WithUri(uri));
         }
+
+        /// <summary>
+        /// Add, update, or remove a parameter from the query string of the <see cref="HttpRequestMessage.RequestUri"/>
+        /// for an HTTP API request.
+        /// </summary>
+        /// <typeparam name="T">The specific type of HTTP API request.</typeparam>
+        /// <param name="apiRequest">The prepared HTTP API request.</param>
+        /// <param name="parameter">The name of the query parameter to add or update.</param>
+        /// <param name="value">
+        /// <para>The value of the query parameter.</para>
+        /// <para>-or-</para>
+        /// <para><see langword="null"/> to remove the query parameter (if present) from the request URI.</para>
+        /// </param>
+        /// <returns>Returns the input argument <paramref name="apiRequest"/>, which was modified according to the
+        /// specified parameter value.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <para>If <paramref name="apiRequest"/> is <see langword="null"/>.</para>
+        /// <para>-or-</para>
+        /// <para>If <paramref name="parameter"/> is <see langword="null"/>.</para>
+        /// </exception>
+        /// <exception cref="ArgumentException">If <paramref name="parameter"/> is empty.</exception>
+        /// <exception cref="ObjectDisposedException">If the HTTP API call has been disposed.</exception>
+        /// <exception cref="InvalidOperationException">If the HTTP API call has already been sent.</exception>
+        public static T WithQueryParameter<T>(this T apiRequest, string parameter, string value)
+            where T : IHttpApiRequest
+        {
+            if (apiRequest == null)
+                throw new ArgumentNullException("apiRequest");
+            if (parameter == null)
+                throw new ArgumentNullException("parameter");
+            if (string.IsNullOrEmpty(parameter))
+                throw new ArgumentException("parameter cannot be empty", "parameter");
+
+            Uri requestUri = apiRequest.RequestMessage.RequestUri;
+            if (value == null)
+                requestUri = UriUtility.RemoveQueryParameter(requestUri, parameter);
+            else
+                requestUri = UriUtility.SetQueryParameter(requestUri, parameter, value);
+
+            apiRequest.RequestMessage.RequestUri = requestUri;
+            return apiRequest;
+        }
+
+        /// <summary>
+        /// Add, update, or remove a parameter from the query string of the <see cref="HttpRequestMessage.RequestUri"/>
+        /// for an HTTP API request.
+        /// </summary>
+        /// <remarks>
+        /// <para>This method simplifies the use of <see cref="WithQueryParameter{T}(T, string, string)"/> in scenarios
+        /// where <see langword="async/await"/> are not used for the preparation of the HTTP API request.</para>
+        /// </remarks>
+        /// <typeparam name="T">The specific type of HTTP API request.</typeparam>
+        /// <param name="apiRequestTask">A <see cref="Task"/> representing an asynchronous operation to prepare the HTTP
+        /// API request.</param>
+        /// <param name="parameter">The name of the query parameter to add or update.</param>
+        /// <param name="value">
+        /// <para>The value of the query parameter.</para>
+        /// <para>-or-</para>
+        /// <para><see langword="null"/> to remove the query parameter (if present) from the request URI.</para>
+        /// </param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation. When the task completes successfully,
+        /// the <see cref="Task{TResult}.Result"/> property contains the result of the input task
+        /// <paramref name="apiRequestTask"/>, which was modified according to the specified
+        /// <paramref name="parameter"/> and <paramref name="value"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <para>If <paramref name="apiRequestTask"/> is <see langword="null"/>.</para>
+        /// <para>-or-</para>
+        /// <para>If <paramref name="parameter"/> is <see langword="null"/>.</para>
+        /// <para>-or-</para>
+        /// <para>If <paramref name="value"/> is <see langword="null"/>.</para>
+        /// </exception>
+        /// <exception cref="ArgumentException">If <paramref name="parameter"/> is empty.</exception>
+        /// <exception cref="ObjectDisposedException">If the HTTP API call has been disposed.</exception>
+        /// <exception cref="InvalidOperationException">If the HTTP API call has already been sent.</exception>
+        public static Task<T> WithQueryParameter<T>(this Task<T> apiRequestTask, string parameter, string value)
+            where T : IHttpApiRequest
+        {
+            if (apiRequestTask == null)
+                throw new ArgumentNullException("apiRequestTask");
+
+            return apiRequestTask.Select(task => task.Result.WithQueryParameter(parameter, value));
+        }
     }
 }
