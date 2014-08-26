55384fc493195e739a6b7612a4ea09d4125da067
Sam Harwell <sam.harwell@rackspace.com>
2014-08-26 08:36:45 -0500

    Rename WithLimit to WithPageSize

--- src/OpenStack.Net/OpenStack/Services/Identity/V2/ListTenantsApiCallExtensions.cs
+++ src/OpenStack.Net/OpenStack/Services/Identity/V2/ListTenantsApiCallExtensions.cs
@@ -17,33 +17,33 @@
         /// Sets (or removes) the <c>limit</c> query parameter for a <see cref="ListTenantsApiCall"/> HTTP API call.
         /// </summary>
         /// <param name="apiCall">The prepared HTTP API call.</param>
-        /// <param name="limit">
+        /// <param name="pageSize">
         /// <para>The maximum number of items to return in a single page of results.</para>
         /// <para>-or-</para>
         /// <para><see langword="null"/> to remove the <c>limit</c> query parameter and use the default page size
         /// configured by the vendor for the service.</para>
         /// </param>
         /// <returns>Returns the input argument <paramref name="apiCall"/>, which was modified according to the
-        /// specified <paramref name="limit"/>.</returns>
+        /// specified <paramref name="pageSize"/>.</returns>
         /// <exception cref="ArgumentNullException">
         /// If <paramref name="apiCall"/> is <see langword="null"/>.
         /// </exception>
-        /// <exception cref="ArgumentOutOfRangeException">If <paramref name="limit"/> is less than 0.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">If <paramref name="pageSize"/> is less than 0.</exception>
         /// <exception cref="ObjectDisposedException">If the HTTP API call has been disposed.</exception>
         /// <exception cref="InvalidOperationException">If the HTTP API call has already been sent.</exception>
         /// <seealso href="http://docs.openstack.org/api/openstack-identity-service/2.0/content/Paginated_Collections-d1e325.html">Paginated collections (OpenStack Identity API v2.0 Reference)</seealso>
-        public static ListTenantsApiCall WithLimit(this ListTenantsApiCall apiCall, int? limit)
+        public static ListTenantsApiCall WithPageSize(this ListTenantsApiCall apiCall, int? pageSize)
         {
             if (apiCall == null)
                 throw new ArgumentNullException("apiCall");
-            if (limit <= 0)
-                throw new ArgumentOutOfRangeException("limit");
+            if (pageSize <= 0)
+                throw new ArgumentOutOfRangeException("pageSize");
 
             Uri uri = apiCall.RequestMessage.RequestUri;
-            if (!limit.HasValue)
+            if (!pageSize.HasValue)
                 apiCall.RequestMessage.RequestUri = UriUtility.RemoveQueryParameter(uri, "limit");
             else
-                apiCall.RequestMessage.RequestUri = UriUtility.SetQueryParameter(uri, "limit", limit.ToString());
+                apiCall.RequestMessage.RequestUri = UriUtility.SetQueryParameter(uri, "limit", pageSize.ToString());
 
             return apiCall;
         }
@@ -86,12 +86,12 @@
         /// Sets (or removes) the <c>limit</c> query parameter for a <see cref="ListTenantsApiCall"/> HTTP API call.
         /// </summary>
         /// <remarks>
-        /// <para>This method simplifies the use of <see cref="WithLimit(ListTenantsApiCall, int?)"/> in
+        /// <para>This method simplifies the use of <see cref="WithPageSize(ListTenantsApiCall, int?)"/> in
         /// scenarios where <see langword="async/await"/> are not used for the preparation of the HTTP API call.</para>
         /// </remarks>
         /// <param name="apiCall">A <see cref="Task"/> representing the asynchronous operation to prepare the HTTP API
         /// call.</param>
-        /// <param name="limit">
+        /// <param name="pageSize">
         /// <para>The maximum number of items to return in a single page of results.</para>
         /// <para>-or-</para>
         /// <para><see langword="null"/> to remove the <c>limit</c> query parameter and use the default page size
@@ -100,17 +100,17 @@
         /// <returns>A <see cref="Task"/> representing the asynchronous operation. When the task completes successfully,
         /// the <see cref="Task{TResult}.Result"/> property contains the result of the input task
         /// <paramref name="apiCall"/>, which was modified according to the specified
-        /// <paramref name="limit"/>.</returns>
+        /// <paramref name="pageSize"/>.</returns>
         /// <exception cref="ArgumentNullException">
         /// If <paramref name="apiCall"/> is <see langword="null"/>.
         /// </exception>
-        /// <exception cref="ArgumentOutOfRangeException">If <paramref name="limit"/> is less than 0.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">If <paramref name="pageSize"/> is less than 0.</exception>
         /// <exception cref="ObjectDisposedException">If the HTTP API call has been disposed.</exception>
         /// <exception cref="InvalidOperationException">If the HTTP API call has already been sent.</exception>
         /// <seealso href="http://docs.openstack.org/api/openstack-identity-service/2.0/content/Paginated_Collections-d1e325.html">Paginated collections (OpenStack Identity API v2.0 Reference)</seealso>
-        public static Task<ListTenantsApiCall> WithLimit(this Task<ListTenantsApiCall> apiCall, int? limit)
+        public static Task<ListTenantsApiCall> WithPageSize(this Task<ListTenantsApiCall> apiCall, int? pageSize)
         {
-            return apiCall.Select(task => apiCall.Result.WithLimit(limit));
+            return apiCall.Select(task => apiCall.Result.WithPageSize(pageSize));
         }
 
         /// <summary>
