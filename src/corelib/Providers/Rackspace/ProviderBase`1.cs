ba084b355a7f08b82bdda5e027b61b89901495e4
Sam Harwell <sam.harwell@rackspace.com>
2014-09-15 06:57:12 -0500

    Updated documentation for BuildDefaultRequestSettings to correctly reflect the default values

--- src/corelib/Providers/Rackspace/ProviderBase`1.cs
+++ src/corelib/Providers/Rackspace/ProviderBase`1.cs
@@ -606,10 +606,11 @@ namespace net.openstack.Providers.Rackspace
         /// The base implementation returns a <see cref="JsonRequestSettings"/> object initialized with the following values.
         ///
         /// <list type="bullet">
-        /// <item>The <see cref="RequestSettings.RetryCount"/> is 2.</item>
+        /// <item>The <see cref="RequestSettings.RetryCount"/> is 0.</item>
         /// <item>The <see cref="RequestSettings.RetryDelay"/> is 200 milliseconds.</item>
         /// <item>The <see cref="RequestSettings.Non200SuccessCodes"/> contains <see cref="HttpStatusCode.Unauthorized"/> and <see cref="HttpStatusCode.Conflict"/>, along with the values in <paramref name="non200SuccessCodes"/> (if any).</item>
         /// <item>The <see cref="RequestSettings.UserAgent"/> is set to <see cref="UserAgentGenerator.UserAgent"/>.</item>
+        /// <item>The <see cref="RequestSettings.ConnectionLimit"/> is set to <see cref="ConnectionLimit"/>.</item>
         /// <item>Other properties are set to the default values for <see cref="JsonRequestSettings"/>.</item>
         /// </list>
         ///
