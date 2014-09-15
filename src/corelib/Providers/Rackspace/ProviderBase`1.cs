7bdfadd55cb507886fa712a159ebf373f4828450
Sam Harwell <sam.harwell@rackspace.com>
2014-09-15 06:57:48 -0500

    Include a Content-Length header with PUT/POST requests that do not include a body (fixes #422)

--- src/corelib/Providers/Rackspace/ProviderBase`1.cs
+++ src/corelib/Providers/Rackspace/ProviderBase`1.cs
@@ -610,6 +610,7 @@ namespace net.openstack.Providers.Rackspace
         /// <item>The <see cref="RequestSettings.RetryDelay"/> is 200 milliseconds.</item>
         /// <item>The <see cref="RequestSettings.Non200SuccessCodes"/> contains <see cref="HttpStatusCode.Unauthorized"/> and <see cref="HttpStatusCode.Conflict"/>, along with the values in <paramref name="non200SuccessCodes"/> (if any).</item>
         /// <item>The <see cref="RequestSettings.UserAgent"/> is set to <see cref="UserAgentGenerator.UserAgent"/>.</item>
+        /// <item>The <see cref="RequestSettings.AllowZeroContentLength"/> is set to <see langword="true"/>.</item>
         /// <item>The <see cref="RequestSettings.ConnectionLimit"/> is set to <see cref="ConnectionLimit"/>.</item>
         /// <item>Other properties are set to the default values for <see cref="JsonRequestSettings"/>.</item>
         /// </list>
@@ -630,6 +631,7 @@ namespace net.openstack.Providers.Rackspace
                 RetryDelay = TimeSpan.FromMilliseconds(200),
                 Non200SuccessCodes = non200SuccessCodesAggregate,
                 UserAgent = UserAgentGenerator.UserAgent,
+                AllowZeroContentLength = true,
                 ConnectionLimit = ConnectionLimit,
             };
         }
