1bb30f46b43b705a55c337dada1c128958c5820e
Sam Harwell <sam.harwell@rackspace.com>
2015-02-10 07:55:11 -0600

    Include SDK V1 keys in source control

--- build/keys.ps1
+++ build/keys.ps1
@@ -5,8 +5,8 @@ If ($Version.Contains('-')) {
 
 	# Use the development keys
 	$Keys = @{
-		'v3.5' = '8965cea5c205d3a3'
-		'v4.0' = '8965cea5c205d3a3'
+		'v3.5' = 'a7b9d24dda86d33e'
+		'v4.0' = 'a7b9d24dda86d33e'
 	}
 
 } Else {
