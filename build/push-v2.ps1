0514b28172cca4f122532e15c960285eb4848cc2
Carolyn Van Slyck <me@carolynvanslyck.com>
2015-06-10 10:39:40 -0500

    Move v1 and v2 to separate branches

--- build/push-v2.ps1
+++
@@ -1,8 +0,0 @@
-. .\version.ps1
-
-If ($VersionV2.EndsWith('-dev')) {
-	$host.ui.WriteErrorLine("Cannot push development version '$VersionV2' to NuGet.")
-	Exit 1
-}
-
-..\src\.nuget\NuGet.exe 'push' ".\nuget\OpenStack.Net.$VersionV2.nupkg"
--- build/version.ps1
+++ build/version.ps1
@@ -1,2 +1,1 @@
 $Version = "1.3.5.0-dev"
-$VersionV2 = "2.0.0.0-dev"
