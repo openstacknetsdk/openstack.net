72a3900a476fd0a324b01399356f83efed6aa1ac
Carolyn Van Slyck <me@carolynvanslyck.com>
2015-07-09 10:02:02 -0500

    Bump to v1.4.0.0

--- src/corelib/Properties/AssemblyInfo.cs
+++ src/corelib/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("openstack.net")]
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -24,9 +24,9 @@ using System.Runtime.InteropServices;
 
 // Refer to the following issue before changing these version numbers:
 // https://github.com/JSIStudios/SimpleRestServices/issues/53
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.6.1")]
-[assembly: AssemblyInformationalVersion("1.3.6.1")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyInformationalVersion("1.4.0.0")]
 
 [assembly: InternalsVisibleTo("OpenStackNet.Testing.Integration, PublicKey=002400000480000094000000060200000024000052534131000400000100010097de8570a3c78aa87191a604e55b95acf1a5075a317d622ab84287f8267b45755a46894b653c8b72d6d38815d2fa2dde8ba5d61c26ce8d5f398dc74ac3eb945fe1c3447753bdf4a3bfc1de3a489c4b5fb82f4cdc67681efffbd627ceca15531f638db991a702d7b8ad6825b51a151db543bad746a157b59ef325e0f44fe1bed1")]
 [assembly: InternalsVisibleTo("OpenStackNet.Testing.Unit, PublicKey=002400000480000094000000060200000024000052534131000400000100010097de8570a3c78aa87191a604e55b95acf1a5075a317d622ab84287f8267b45755a46894b653c8b72d6d38815d2fa2dde8ba5d61c26ce8d5f398dc74ac3eb945fe1c3447753bdf4a3bfc1de3a489c4b5fb82f4cdc67681efffbd627ceca15531f638db991a702d7b8ad6825b51a151db543bad746a157b59ef325e0f44fe1bed1")]
