cb755dad39c063fd8e957022e66a7798705fc178
Carolyn Van Slyck <me@carolynvanslyck.com>
2015-08-10 15:27:09 -0500

    Do not run integration tests in parallel

--- src/testing/integration/Properties/AssemblyInfo.cs
+++ src/testing/integration/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -33,3 +34,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Xunit logging doesn't work with running tests in parallel
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
