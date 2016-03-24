ae5d303d0d1665fca8da3a304fe72732a6381b85
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-03-24 15:08:48 -0500

    Add layout for floating ip test

---
+++ src/testing/integration/Networking/v2/Layer3/Layer3ExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenStack.Networking.v2.Layer3
+{
+    public class Layer3ExtensionTests
+    {
+/*
+1. Make an internal network and subnet.
+2. Make an external network and submit.
+3. Make a router on the external network.
+4. Attach the router to the internal network.
+5. Make a floating ip on the external network.
+6. Make a server on the internal network.
+7. Associate the floating ip to the server.
+*/
+    }
+}
